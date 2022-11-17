1aa36668fb91bc606bf1c4fb4ee0c3bbf5d3ab0a
tamasgyetvan <117265815+tamasgyetvan@users.noreply.github.com>
2022-11-17 16:29:00 +0100

    getPlayerChoice updated to case insensitive version, playRound function was added to the script file

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function playRound(playerSelection, computerSelection) {
-    
 
+    if (playerSelection == computerSelection) {
+
+        console.log("Draw")
+
+    }
+    
+    if ((playerSelection == "Rock" && computerSelection =="Paper") || 
+        (playerSelection == "Paper" && computerSelection =="Scissors") ||
+        (playerSelection == "Scissors" && computerSelection == "Rock")) {
+
+            console.log("You Lose!")
+        }
+
+    if ((playerSelection == "Rock" && computerSelection =="Scissors") || 
+        (playerSelection == "Paper" && computerSelection =="Rock") ||
+        (playerSelection == "Scissors" && computerSelection == "Paper")) {
+
+            console.log("You win!")
+        }
+  
+
+        
 }
 
 function getComputerChoice() {
@@ -15,12 +36,20 @@ function getPlayerChoice() {
     let playerChoice = String(prompt("Enter your choice:"))
 
     if (["rock", "paper", "scissors"].includes(playerChoice.toLowerCase())) {
-        return playerChoice
+        return playerChoice.charAt(0).toLocaleUpperCase() + playerChoice.slice(1)
     } else {
-        console.log("Wrong input, please enter a valid input!")
-
+        console.log("Wrong input, please reload the page and enter a valid input!")
+        
     }
+
 }
+
+let x = getPlayerChoice();
+let y = getComputerChoice();
+
+console.log("Player choice: " + x)
+console.log("Computer Choice: " + y)
+playRound(x, y);
 
 
 
